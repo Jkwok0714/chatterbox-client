7c4c5ef7f6158c21e5aa8da51470367f3bf533e2
Hack Reactor Student <student@hackreactor.com>
2017-09-01 18:43:46 -0700

    Added setInterval, html escaping, corrected fetch method

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,15 +1,16 @@
 // YOUR CODE HERE:
 var app = {};
 app.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
-app.send = (message) => {
+app.send = () => {
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: app.server,
     type: 'POST',
-    data: JSON.stringify(message),
+    data: JSON.stringify(app.buildMessageObject()),
     contentType: 'application/json',
     success: function (data) {
-      console.log('chatterbox: Message sent');
+
+      console.log('chatterbox: Message sent', data);
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -18,24 +19,48 @@ app.send = (message) => {
   });
 };
 
-app.fetch = () => {
-  var chatHistory = [];
-  $.get(app.server, (data) => {
-    var messageArray = data.results;
-    for (var message of messageArray) {
-      app.renderMessage(message);
-    }
-
-  });
-  // console.log(chatHistory);
-  return chatHistory;
-};
-
 app.clearMessages = () => {
   $('#chats').empty();
 };
 
+app.fetch = () => {
+  // $.get(app.server, (data) => {
+  //   console.log('suhhh', data);
+  //   var messageArray = data.results;
+  //   for (var message of messageArray) {
+  //     app.renderMessage(message);
+  //   }
+  // });
+  app.clearMessages();
+  var d = new Date( (new Date()).getTime() - 1000 * 6000);
+  var dateISO = d.toISOString();
+
+  console.log(dateISO);
+
+  $.ajax({
+    url: app.server,
+    type: 'GET',
+    // beforeSend: app.setHeader,
+    data: `where={"createdAt":{"$gte":{"__type":"Date","iso":"${dateISO}"}}}`,
+    success: function(data) {
+      console.log(data);
+        var messageArray = data.results;
+        for (var message of messageArray) {
+          app.renderMessage(message);
+        }
+    }
+  });
+};
+
+// app.setHeader = function(xhr) {
+//   xhr.setRequestHeader('where={"createdAt":{"$gte":{"__type":"Date","iso":"2017-07-07"}}}', '');
+// }
+
+
 app.renderMessage = (message) => {
+  if (message.username === null && message.text === null) {
+    return;
+  }
   var $body = $('#chats');
   //var $textBit = $(`<div class="message"><button class="username">${message.username}</button>
   //  <br>${message.text}</div>`);
@@ -44,9 +69,12 @@ app.renderMessage = (message) => {
   $innerButton.addClass('username');
   $innerButton.text(message.username);
   $textBit.addClass('message');
+  $textBit.hide();
   $textBit.text(message.text);
-  $textBit.appendTo($body);
+  $textBit.prependTo($body);
+  $textBit.addClass(message.roomname);
   $innerButton.prependTo($textBit);
+  $textBit.show();
 };
 
 app.renderRoom = (room) => {
@@ -60,17 +88,20 @@ app.handleUsernameClick = () => {
 };
 
 app.handleSubmit = () => {
-  // var message = {
-  //   username: 'Mel Brooks',
-  //   text: 'It\'s good to be the king',
-  //   roomname: 'lobby'
-  // };
-  // app.send(message);
-  // console.log('submit');
-  return true;
+
+  app.send();
 };
 
-
+app.buildMessageObject = function () {
+  var message = {
+    username: (window.location.search).split('=')[1],
+    text: $('#message').val(),
+    roomname: 'lobby'
+  };
+  $('#message').val('');
+  app.renderMessage(message);
+  return message;
+}
 
 app.init = () => {
   var chatHistory = app.fetch();
@@ -80,15 +111,23 @@ app.init = () => {
   $('#chats').on('click', '.username', () => {
     app.handleUsernameClick();
   });
-  $('.submit').off().submit((e) => {
-    e.preventDefault();
+  $('#send').off().submit('click', function (e) {
+    // e.stopPropagation();
     app.handleSubmit();
+    e.preventDefault();
   });
-
-  $('#send .submit').val('BOOM');
 
 };
 
 $(document).ready(() => {
   app.init();
+  var timer = setInterval(app.fetch ,10000)
+
+  console.log(window.location.search);
+  console.log($('#roomSelect').val());
+
+  // $.ajaxPrefilter(function (settings, _, jqXHR) {
+  //   jqXHR.setRequestHeader('__type', 'Date');
+  //   // jqXHR.setRequestHeader('iso', '2017-07-07');
+  // });
 });
